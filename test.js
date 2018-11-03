f34dea36521d8e72171dbc02992e622116e74b7b
Franziska Hinkelmann <franzih@chromium.org>
2018-11-03 09:06:16 -0400

    Install mocha, add dummy test

---
+++ test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('Git bisect demo', function() {
+    describe('#foo()', function() {
+        it('should not do anything', function() {
+            assert(true);
+        });
+    });
+});
+
