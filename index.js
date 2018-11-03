1697fc88a3b2124cc00df3ca5a1a66df11492730
Franziska Hinkelmann <franzih@chromium.org>
2018-11-03 09:27:55 -0400

    Explain logic() function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// Remember to comment your code
+// This function encapsulates obsure domain
+// logic. At this point, it is unclear why
+// it does what it does. But there are numerous
+// test asserting its correctness.
 let logic = (a, b) => {
 	return Math.abs(a + b);
 };
