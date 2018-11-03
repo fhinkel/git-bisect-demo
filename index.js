16a74f57873ff63043c5b846c2e91eed4e64ceb6
Franziska Hinkelmann <franzih@chromium.org>
2018-11-03 09:34:04 -0400

    Add more comments
    Drive-by fix: Small refactoring.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // This function encapsulates obsure domain
 // logic. At this point, it is unclear why
 // it does what it does. But there are numerous
-// test asserting its correctness.
+// test asserting its correctness. It is the core
+// part of our business.
 let logic = (a, b) => {
-	return Math.abs(a + b);
+	return a + b;
 };
 
 module.exports = logic;
